364ea55dbe3c81b70df80cfd5ef0c461ba006ebd
Trend20 <enockomondi305@gmail.com>
2023-07-31 23:30:34 +0300

    adding category routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,37 @@ import Products from './pages/products/Products';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Contact from "./components/contact/Contact";
+import Category from "./components/category/Category";
+import Accessories from "./components/category/accessories/Accessories";
+import Sportsware from "./components/category/sportsware/Sportsware";
+import Jewelry from "./components/category/jewelry/Jewelry";
+import Outware from "./components/category/outware/Outware";
+import Womans from "./components/category/womans/Womans";
+import Footware from "./components/category/footware/Footware";
+import Mens from "./components/category/mens/Mens";
+import Outdoor from "./components/category/outdoor/Outdoor";
 
 const App = ():JSX.Element  =>{
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
+        <Category />
         <Routes>
           <Route path="/login"  element={<Login />}/>
           <Route path="/register" element={<Register />} />
           <Route path="/products" element={<Products />} />
           <Route path="/contacts" element={<Contact />} />
-          <Route path="/blog" element={<Products />} />
+          <Route path="/store/category/accessories" element={<Accessories />} />
+          <Route path="/store/category/outdoor" element={<Outdoor />} />
+          <Route path="/store/category/mens" element={<Mens />} />
+          <Route path="/store/category/footware" element={<Footware />} />
+          <Route path="/store/category/womans" element={<Womans />} />
+          <Route path="/store/category/outware" element={<Outware />} />
+          <Route path="/store/category/jewelry" element={<Jewelry />} />
+          <Route path="/store/category/sportsware" element={<Sportsware />} />
+          {/*<Route path="/blog" element={<Products />} />*/}
+          {/*<Route path="/blog" element={<Products />} />*/}
         </Routes>
         <Footer />
       </div>
