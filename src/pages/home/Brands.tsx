6abb893552158ed5b12cc63ca20c42337f628432
Trend20 <enockomondi305@gmail.com>
2023-08-02 16:13:02 +0300

    adding brands component

---
+++ src/pages/home/Brands.tsx
@@ -0,0 +1,69 @@
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Info from '../../assests/data/application.json'
+
+export interface Brand{
+    img: string,
+    id:number,
+    name: string
+}
+
+export type Options={
+    margin: number,
+    responsiveClass: boolean,
+    autoWidth: boolean,
+    loop: boolean
+    pullDrag: boolean
+    autoplay: boolean
+    autoplayHoverPause: boolean
+    autoHeight: boolean
+    smartSpeed: number
+    responsive: any
+}
+const Brands = ():JSX.Element =>{
+    const options: Options = {
+        margin: 38,
+        responsiveClass: true,
+        autoWidth: true,
+        loop: true,
+        pullDrag: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoHeight: false,
+        smartSpeed: 800,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            400: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            700: {
+                items: 3,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    };
+
+    return(
+        <div className='brands'>
+            <OwlCarousel className='owl-carousel' {...options}>
+                {Info.brands.map((brand: Brand) => {
+                    return (
+                        <div key={brand.id}>
+                            <img src={brand.img} alt={brand.name} />
+                        </div>
+                    );
+                })}
+            </OwlCarousel>
+        </div>
+    )
+}
+
+export default  Brands;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
 import './Home.css';
+import Brands from "./Brands";
 const Home = () =>{
     return(
         <div className='home'>
             <Hero />
+            <Brands />
         </div>
     )
 }
