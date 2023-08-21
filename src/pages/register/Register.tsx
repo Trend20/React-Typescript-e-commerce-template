2443fc02b0e2a4a4442d5bec074108845b88e756
Trend20 <enockomondi305@gmail.com>
2023-08-21 19:07:06 +0300

    adding register page template

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='register-page'>
+      <h1>Register</h1>
+      <form action="">
+        <div className="input-field">
+          <input type="text" placeholder='First Name' />
+        </div>
+        <div className="input-field">
+          <input type="text" placeholder='Last Name' />
+        </div>
+        <div className="input-field">
+          <input type="email" placeholder='Email' />
+        </div>
+        <div className="input-field">
+          <input type="password" placeholder='Password' />
+        </div>
+        <div className="input-field">
+          <input type="password" placeholder='Repeat Password' />
+        </div>
+      </form>
+    </div>
   )
 }
 
