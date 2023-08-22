01891a3c48845caf8b62a88762d804cea0af371a
Trend20 <enockomondi305@gmail.com>
2023-08-22 22:13:07 +0300

    register form state

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import { Link} from "react-router-dom";
 
 const Register = ():JSX.Element => {
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [registerEmail, setRegisterEmail] = useState<string>('')
+  const [registerPassword, setRegisterPassword] = useState<string>('')
+  const [repeatPassword, setRepeatPassword] = useState<string>('')
   return (
     <div className='register'>
       {/*<h1>Register</h1>*/}
       <form action="">
         <div className="input-field">
-          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='First Name' value={firstName} onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}/>
         </div>
         <div className="input-field">
-          <input type="text" placeholder='Last Name' />
+          <input type="text" placeholder='Last Name' value={lastName} onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}/>
         </div>
         <div className="input-field">
-          <input type="email" placeholder='Email' />
+          <input type="email" placeholder='Email' value={registerEmail} onChange={(e: ChangeEvent<HTMLInputElement>) => setRegisterEmail(e.target.value)}/>
         </div>
         <div className="input-field">
-          <input type="password" placeholder='Password' />
+          <input type="password" placeholder='Password' value={registerPassword} onChange={(e: ChangeEvent<HTMLInputElement>) => setRegisterPassword(e.target.value)}/>
         </div>
         <div className="input-field">
-          <input type="password" placeholder='Repeat Password' />
+          <input type="password" placeholder='Repeat Password' value={repeatPassword} onChange={(e: ChangeEvent<HTMLInputElement>) => setRepeatPassword(e.target.value)}/>
         </div>
         <div className="register-actions">
           <p>Already registered?  <Link to='/login' style={{textDecoration:'none'}}>Login</Link></p>
