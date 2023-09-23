abba8afd490c59efb7e8ad3a8fd6b0ceb5b302ee
Trend20 <enockomondi305@gmail.com>
2023-09-23 09:31:45 +0300

    adding supabase configs

---
+++ src/lib/api.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+import { REACT_SUPABASE_URL, REACT_SUPABASE_KEY} from "./constants";
+
+// export const supabase = createClient(REACT_SUPABASE_URL, REACT_SUPABASE_KEY);
+
+
+
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const REACT_SUPABASE_URL = process.env.REACT_SUPABASE_URL;
+export const REACT_SUPABASE_KEY = process.env.REACT_SUPABASE_ANON_KEY;
