57260304a0339d60c89a138f09f7498ec16dfee2
Trend20 <enockomondi305@gmail.com>
2024-06-03 09:52:41 +0300

    small ui fix

--- src/assets/data/popular.ts
+++ src/assets/data/popular.ts
@@ -10,19 +10,19 @@ export const popularProducts: Product[] = [
   {
     img: popularOne,
     description: 'Women’s Crew Neck Short Sleeve T-Shirt',
-    price: '$41.00',
+    price: '41.00',
     id: 1
   },
   {
     img: popularTwo,
     description: 'Ripped Jeans Denim Jeggings Trousers Skinny',
-    price: '$80.00',
+    price: '80.00',
     id: 2
   },
   {
     img: popularThree,
     description: 'Jeans Free Suspenders Light Blue Denim Pants',
-    price: '$99.00',
+    price: '99.00',
     id: 3
   }
 ]
@@ -32,19 +32,19 @@ export const onsaleProducts: Product[] = [
   {
     img: popularTwo,
     description: 'Ripped Jeans Denim Jeggings Trousers Skinny',
-    price: '$80.00',
+    price: '80.00',
     id: 1
   },
   {
     img: onsaleOne,
     description: 'Women’s Vicki Straight Jeans Blue',
-    price: '$90.00',
+    price: '90.00',
     id: 2
   },
   {
     img: onsaleTwo,
     description: 'Women’s Sleeveless Faux Wrap Dress',
-    price: '$82.00',
+    price: '82.00',
     id: 3
   }
 ]
