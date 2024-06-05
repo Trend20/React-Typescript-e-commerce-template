537abee9d680e757ef67de8ea801ed50ab76d61f
Trend20 <enockomondi305@gmail.com>
2024-06-05 13:37:44 +0300

    adding empty cart UI

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -2,38 +2,41 @@ import Checkout from "./Checkout";
 import SubTotal from "./SubTotal";
 import {useSelector} from "react-redux";
 import CartItem from "./CartItem";
+import EmptyCart from "./EmptyCart";
 
 const CartPage = () =>{
     const {items} = useSelector((store:any) => store.cart);
     return(
     <section>
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-            <div className="mx-auto max-w-3xl">
-                <header className="text-center">
-                    <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">Your Cart</h1>
-                </header>
+        {
+            items.length !== 0 ? <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <div className="mx-auto max-w-3xl">
+                    <header className="text-center">
+                        <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">Your Cart</h1>
+                    </header>
 
-                <div className="mt-8">
-                    <ul className="space-y-4">
-                        {
-                            items.map((item:any) => (
-                                <CartItem key={item.id} item={item} />
-                            ))
-                        }
-                    </ul>
+                    <div className="mt-8">
+                        <ul className="space-y-4">
+                            {
+                                items.map((item: any) => (
+                                    <CartItem key={item.id} item={item}/>
+                                ))
+                            }
+                        </ul>
 
-                    <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
-                        <div className="w-screen max-w-lg space-y-4">
-                            <SubTotal />
-                            <Checkout />
+                        <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
+                            <div className="w-screen max-w-lg space-y-4">
+                                <SubTotal/>
+                                <Checkout/>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div> : <EmptyCart />
+        }
     </section>
 
-)
+    )
 }
 
 export default CartPage;
--- src/pages/cart/EmptyCart.tsx
+++ src/pages/cart/EmptyCart.tsx
@@ -1,12 +1,13 @@
 import emptyCartImg from '../../assets/img/cart.svg'
+import {Link} from "react-router-dom";
 
 const EmptyCart = () => {
     return (
-        <div className="flex flex-col justify-center items-center w-full md:w-1/2">
-            <img src={emptyCartImg} alt="cart-img" />
-            <p>Your cart is empty!</p>
-            <p>Browse our categories and discover our best deals!</p>
-            <button>Start Shopping</button>
+        <div className="flex flex-col justify-center items-center w-full py-16 space-y-2">
+            <img src={emptyCartImg} alt="cart-img" className="w-28 h-28" />
+            <p className="text-xl font-semibold">Your cart is empty!</p>
+            <p className="text-sm">Browse our categories and discover our best deals!</p>
+            <Link to="/products" className="rounded-md bg-[#023047] text-white px-4 py-2">Start Shopping</Link>
         </div>
     )
 }
