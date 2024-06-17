e5e01468e4f69d80f5e690f0dc9f7de84878b6b0
Trend20 <enockomondi305@gmail.com>
2024-06-17 09:11:13 +0300

    adding the checkout page route

--- src/pages/cart/Checkout.tsx
+++ src/pages/cart/Checkout.tsx
@@ -1,7 +1,11 @@
+import {useNavigate} from "react-router-dom";
+
 const Checkout = () =>{
+    const navigate = useNavigate();
     return (
         <div className="flex justify-end">
             <button
+                onClick={() => navigate('/cart/checkout')}
                 className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
             >
                 Checkout
