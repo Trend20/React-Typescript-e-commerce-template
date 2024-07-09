9e7b63e453d41846325e02f209b2fe52a7ab61b7
Trend20 <enockomondi305@gmail.com>
2024-07-09 14:54:23 +0300

    adding the features section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,11 +5,13 @@ import PopularProducts from './PopularProducts'
 import Deals from './Deals'
 import ProductsOnSale from './ProductsOnSale'
 import Showcase from './Showcase'
+import Features from "../../components/features/Features";
 const Home = (): JSX.Element => {
   return (
         <div className='home'>
             <Hero />
             <Brands />
+            <Features />
             <PopularProducts />
             <Deals />
             <ProductsOnSale />
